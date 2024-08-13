4d09824fecd9e809bdf8b11e224c936c57479de7
amanjdj <amanjdj@outlook.com>
2024-08-13 16:06:10 +0530

    FastReactCo

---
+++ src/features/cart/Cart.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+// import { Link } from "react-router-dom";
+import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
+import CartItem from "../cart/CartItem";
+import EmptyCart from "../cart/EmptyCart";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart } from "./cartSlice";
+
+function Cart() {
+  // const cart = fakeCart;
+
+  const cart = useSelector(getCart);
+
+  const username = useSelector((state) => state.user.username);
+
+  const dispatch = useDispatch();
+
+  if (!cart.length) return <EmptyCart />;
+
+  return (
+    <div className="px-4 py-3">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
+
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-y-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+
+        <Button type={"secondary"} onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
+        {/* <button>Clear cart</button> */}
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
